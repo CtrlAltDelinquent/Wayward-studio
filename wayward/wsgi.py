752cf1a7ba57337f6950e255d73c002c79f80752
Jacob.Gibbs <211082079+CtrlAltDelinquent@users.noreply.github.com>
2025-11-21 00:54:52 -0600

    Instatiated django project for wayward website

---
+++ wayward/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for wayward project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wayward.settings')
+
+application = get_wsgi_application()
