bd78078a82077abbdd75c8918a1f94aa39c19c94
Jacob.Gibbs <211082079+CtrlAltDelinquent@users.noreply.github.com>
2025-11-21 21:49:11 -0600

    Added view functions for careers and accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# account views
+
+def login_view(request):
+    return HttpResponse("Login page")
+
+def signup_view(request):
+    return HttpResponse("signup page")
+
+def perks_view(request):
+    return HttpResponse("perks page")
+
+
+
--- careers/views.py
+++ careers/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def jobs_list(request):
+    return HttpResponse("Job list page")
+
+def job_detail(request, job_id):
+    return HttpResponse(f"Job detail page ID: {job_id}")
+
+def job_apply(request, job_id):
+    return HttpResponse(f"Apply page for a specific job ID: {job_id}")
