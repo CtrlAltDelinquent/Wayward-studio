32596a78277b903a51cf409fc95d874aa2aa992e
Jacob.Gibbs <211082079+CtrlAltDelinquent@users.noreply.github.com>
2025-11-21 22:00:06 -0600

    rewired homepage route to flow through dedicated app instead of project base to separate business logic of homepage and project globl config settings

--- wayward/urls.py
+++ wayward/urls.py
@@ -25,7 +25,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home"),
+    path('', include('homepage.urls')),
     path('accounts/', include('accounts.urls')),
     path('careers/', include('careers.urls')),
     path('games', include('games.urls')),
--- wayward/views.py
+++ wayward/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Home page")
+# def home(request):
+#     return HttpResponse("Home page")
+
+# FILE DOES NOTHING - IS NOT CONNECTED
