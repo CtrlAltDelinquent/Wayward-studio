e4f20f230c2ea18770c29245362cc3b5a299074f
Jacob.Gibbs <211082079+CtrlAltDelinquent@users.noreply.github.com>
2025-11-25 23:20:10 -0600

    making the html items modular for later more dynamic use. using javascript raw for conditionally rendering logic

--- games/views.py
+++ games/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def games_list(request):
-    return HttpResponse("Finished games list")
+    return render(request, 'games_gallary.html')
 
 def game_detail(request, slug):
     return HttpResponse(f"Game detail placeholder for game: {slug}")
