058dc5501e11578ce18227c76b371ac3ae6479bc
Jacob.Gibbs <211082079+CtrlAltDelinquent@users.noreply.github.com>
2025-11-27 16:41:15 -0600

    careers/views.py

--- careers/views.py
+++ careers/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def jobs_list(request):
-    return HttpResponse("Job list page")
+    return render(request, "careers.html")
 
 def job_detail(request, job_id):
     return HttpResponse(f"Job detail page ID: {job_id}")
